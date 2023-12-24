47407b9a8a02727710ffa8e764532e9bc9cb9e84
xwqianbei <2816739071@qq.com>
2023-12-24 12:40:20 +0800

    补题23/12/22动态规划题

--- Daily/23_12_22_1671/solve_1671.cpp
+++ Daily/23_12_22_1671/solve_1671.cpp
@@ -1,6 +1,34 @@
 class Solution {
 public:
     int minimumMountainRemovals(vector<int>& nums) {
+        int len = nums.size(); 
+        vector<int> dpl(len + 1, 1);
+        vector<int> dpr(len + 1, 1);
 
+        dpl[0] = 1;
+        for(int i = 1; i < len; i++){
+            for(int j = 0; j < i; j++){
+                if(nums[i] > nums[j]){
+                    dpl[i] = max(dpl[i], dpl[j] + 1);
+                }
+            }
+        }
+
+        dpr[len - 1] = 1;
+        for(int i = len - 2; i >= 0; i--){
+            for(int j = len - 1; j > i; j--){
+                if(nums[i] > nums[j]){
+                    dpr[i] = max(dpr[i], dpr[j] + 1);
+                }
+            }
+        }
+
+        int ans = 0;
+        for(int i = 1; i < len - 1; i++){
+            if(dpl[i] != 1 && dpr[i] != 1)
+                ans = max(ans, dpl[i] + dpr[i] - 1);
+        }
+
+        return len - ans;        
     }
 };
